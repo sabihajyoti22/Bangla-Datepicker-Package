a92b7b53c938843e51ebd2fedec31d0b3c88991f
sabihajyoti22 <sabihajyoti@gmail.com>
2024-06-04 21:41:38 +0600

    fix: checking import/export

---
+++ src/script.js
@@ -0,0 +1,176 @@
+document.write(`
+<div id="container">
+        <input readonly id="calendar-input" type="text" placeholder="তারিখ সিলেক্ট করুণ">
+        <img id="calendar-icon" src="../assets/icons/calendar.svg" alt="calendar icon">
+
+        <div id="calendar-card">
+            <div id="card-header">
+                <img id="left-arrow" src="../assets/icons/leftArrow.svg" alt="left arrow">
+                <div>
+                    <div id="month"></div>
+                    <div id="year"></div>
+                </div>
+                <img id="right-arrow" src="../assets/icons/rightArrow.svg" alt="right arrow">
+            </div>
+
+            <div id="weeks">
+                <div>রবি</div>
+                <div>সোম</div>
+                <div>মঙ্গল</div>
+                <div>বুধ</div>
+                <div>বৃহস্প</div>
+                <div>শুক্র</div>
+                <div>শনি</div>
+            </div>
+
+            <div id="days">
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+`)
+const calendarContainer = document.getElementById('container')
+const calendarInput = document.getElementById('calendar-input')
+const calendarCard = document.getElementById('calendar-card')
+const leftArrow = document.getElementById('left-arrow')
+const rightArrow = document.getElementById('right-arrow')
+const month = document.getElementById('month')
+const year = document.getElementById('year')
+const days = document.getElementById('days')
+
+const monthArr = ['জানুয়ারি', 'ফেব্রুয়ারি', 'মার্চ', 'এপ্রিল', 'মে', 'জুন', 'জুলাই', 'আগস্ট', 'সেপ্টেম্বর', 'অক্টোবর', 'নভেম্বর', 'ডিসেম্বর']
+
+let curentMonth = new Date().getMonth()
+let curentYear = new Date().getFullYear()
+
+const convertNumber = (num) => {
+    const bangNum = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯']
+    var converted = ''
+
+    if (isNaN(num)) {
+        return converted
+    }
+
+    num = Number(num)
+    num = num.toString()
+    for (let i = 0; i < num.length; i++) {
+        if (Number.parseInt(num[i]) || Number.parseInt(num[i]) === 0) {
+            converted += bangNum[num[i]]
+        } else {
+            converted += num[i]
+        }
+    }
+
+    return converted
+}
+
+const daysInMonths = () => {
+    return new Date(curentYear, (curentMonth + 1), 0).getDate()
+}
+const startDay = () => {
+    return new Date(curentYear, curentMonth, 1).getDay()
+}
+
+const selectedDate = (evt) => {
+    calendarInput.innerText = curentYear + '-' + curentMonth + '-' + evt.target.value
+    calendarInput.value = evt.target.innerHTML + '/' + convertNumber(curentMonth) + '/' + convertNumber(curentYear)
+    calendarCard.style.display = 'none'
+}
+
+const setCalender = () => {
+    month.innerHTML = monthArr[curentMonth]
+    year.innerHTML = convertNumber(curentYear)
+
+    const nodes = days.children
+    let space = 0
+    let date = 0
+    let spaceCount = startDay()
+    let dates = daysInMonths()
+
+    for (let el = 0; el < nodes.length; el++) {
+        if (space < spaceCount) {
+            nodes[el].innerHTML = ''
+            space++
+        } else if (date < dates) {
+            nodes[el].innerHTML = convertNumber(date + 1)
+            nodes[el].value = date + 1
+            nodes[el].addEventListener("click", (evt) => selectedDate(evt))
+            date++
+        } else {
+            nodes[el].innerHTML = ''
+        }
+    }
+}
+
+setCalender()
+
+rightArrow.addEventListener('click', () => {
+    if (curentMonth >= 11) {
+        curentMonth = 0
+        curentYear++
+    } else {
+        curentMonth++
+    }
+    setCalender()
+})
+
+leftArrow.addEventListener('click', () => {
+    if (curentMonth === 0) {
+        curentMonth = 11
+        curentYear--
+    } else {
+        curentMonth--
+    }
+    setCalender()
+})
+
+calendarInput.addEventListener('focus', () => {
+    calendarCard.style.display = 'block'
+})
+
+// calendarInput.addEventListener('blur', () => {
+//     calendarCard.style.display = 'none'
+// })
+
+export default { calendarContainer }
